c4fc9e7e171a4a7b15880f69e7b48ab958fde7a7
Zakhar Galanin <zakhargalanin@gmail.com>
2022-07-23 21:25:19 -0700

    Add project files.

---
+++ Lab20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab20
+{
+    internal class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = Lenght;
+            double p = Lenght(r);
+            Console.WriteLine($"Длина: {p}");
+            myDelegate = Area;
+            p = Area(r);
+            Console.WriteLine($"Площадь: {p}");
+            myDelegate = Volume;
+            p = Volume(r);
+            Console.WriteLine($"Объем: {p}");
+
+            Console.ReadKey();
+
+
+        }
+        static double Lenght(double r) => 2 * Math.PI * r;
+        static double Area(double r) => Math.PI * r * r;
+        static double Volume(double r) => 4 * Math.PI * r * r * r / 3;
+    }
+}
